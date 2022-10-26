fa94c8e7c72b179aa657445ca3956bf6e31186d4
nesrv <ne_srv@mail.ru>
2022-10-27 01:09:53 +0200

    Part 2. Initial working version without database

--- app/app/settings.py
+++ app/app/settings.py
@@ -24,8 +24,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "app",
     "interface",
-    
 ]
 
 MIDDLEWARE = [
@@ -97,3 +97,31 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+
+from enum import Enum
+from environs import Env
+
+env = Env()
+env.read_env()
+
+# logging
+LOG_LEVEL = env.enum(
+    'LOG_LEVEL',
+    type=Enum('LOG_LEVEL', 'DEBUG INFO WARNING ERROR CRITICAL'),
+    ignore_case=True,
+    default='INFO',
+).name
+
+JWT_ALGORITHM = env.str('JWT_ALGORITHM', 'RS256')
+JWT_PRIVATE_KEY = env.str('JWT_PRIVATE_KEY')
+
+HTTP_LISTEN_HOST = env.str('HTTP_LISTEN_HOST', '127.0.0.1')
+HTTP_LISTEN_PORT = env.int('HTTP_LISTEN_PORT', 8000)
+CONTROLLER_URL = f'http://{HTTP_LISTEN_HOST}:{HTTP_LISTEN_PORT}'
+
+CONTROLLERS = [
+    {'name': "Qotto", 'url': "http://127.0.0.1:8000"},
+    {'name': "RBMG", 'url': "http://127.0.0.1:8001"},
+]
--- app/interface/models.py
+++ app/interface/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+
+
+
+
+class User(models.Model):
+    name = models.CharField('Name', max_length=50)
+    login = models.CharField('', max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Door(models.Model):
+    name = models.CharField('Login', max_length=50)
+    contact = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="doors")
+
+    def __str__(self):
+        return self.name

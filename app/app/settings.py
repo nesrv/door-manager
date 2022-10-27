837dcd2ac790e5e2f8324fd65fd4edccdb5e49d8
nesrv <ne_srv@mail.ru>
2022-10-27 02:31:30 +0200

    Part 3. Database added

--- app/app/settings.py
+++ app/app/settings.py
@@ -62,7 +62,10 @@ WSGI_APPLICATION = "app.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3"
+    }
 }
 
 # Password validation
@@ -93,12 +96,13 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-
 
 from enum import Enum
 from environs import Env
@@ -125,3 +129,6 @@ CONTROLLERS = [
     {'name': "Qotto", 'url': "http://127.0.0.1:8000"},
     {'name': "RBMG", 'url': "http://127.0.0.1:8001"},
 ]
+
+
+
